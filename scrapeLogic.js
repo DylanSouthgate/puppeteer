3d3a7cf398f1e52aab8b4702703cf5b3472e4480
Dylan Southgate <44190055+DylanSouthgate@users.noreply.github.com>
2024-03-11 16:10:21 +0200

    Add files via upload

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -9,26 +9,29 @@ const scrapeLogic = async (res) => {
       "--single-process",
       "--no-zygote",
       // Add argument to load extension
-      `--load-extension=${process.env.PUPPETEER_EXTENSIONS}`,
+      `--load-extension=${path.resolve(process.env.PUPPETEER_EXTENSIONS)}`,
     ],
     executablePath:
       process.env.NODE_ENV === "production"
         ? process.env.PUPPETEER_EXECUTABLE_PATH
         : puppeteer.executablePath(),
   });
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1080, height: 1024 });
   try {
-    const page = await browser.newPage();
 
-    // Check if the extension is loaded
-    const extensions = await page.evaluate(() => {
-      return new Promise(resolve => {
-        chrome.management.getAll(extensions => {
-          resolve(extensions.map(ext => ext.name));
-        });
-      });
-    });
-    console.log("Loaded extensions:", extensions);
-    res.send(extensions);
+    let link = "https://www.watchasian.sk/running-man-2010-episode-695.html";
+    await page.goto(link,{timeout: 0,waitUntil: 'networkidle2'});
+    await page.waitForSelector('.watch_video > iframe');
+    console.log('Selector found: .watch_video > iframe');
+    await page.click('.watch_video > iframe');
+    console.log('Selector clicked');
+
+
+    const htmlContent = await page.content();
+
+
+    res.send(htmlContent);
   } catch (e) {
     console.error(e);
     res.send(`Something went wrong while running Puppeteer: ${e}`);
