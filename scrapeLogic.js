e284c8f74365b2eb35d7419d34dba7698effe44a
Dylan Southgate <44190055+DylanSouthgate@users.noreply.github.com>
2024-03-11 14:50:41 +0200

    Add files via upload

--- scrapeLogic.js
+++ scrapeLogic.js
@@ -3,13 +3,11 @@ require("dotenv").config();
 
 const scrapeLogic = async (res) => {
   const browser = await puppeteer.launch({
-    defaultViewport: null,
     args: [
       "--disable-setuid-sandbox",
       "--no-sandbox",
       "--single-process",
-      "--no-zygote",
-      `--load-extension=${process.env.PUPPETEER_EXTENSIONS}`, // Include loaded extension
+      "--no-zygote"
     ],
     executablePath:
       process.env.NODE_ENV === "production"
@@ -19,27 +17,16 @@ const scrapeLogic = async (res) => {
   try {
     const page = await browser.newPage();
 
-    page.setRequestInterception(true);
-        page.on("request", req => {
-            if (req.url().endsWith(".m3u8"))
-            {
-                console.log(req.url());
-                res.send(req.url());
-                page.close();
-            }
-            if (req.url().endsWith(".png") || req.url().endsWith(".jpg") || req.url().endsWith(".css"))
-            {
-                req.abort();
-            }
-            else{
-                req.continue();
-            }
-        })
+    // Set screen size
+    await page.setViewport({ width: 1080, height: 1024 });
 
     let link = "https://www.watchasian.sk/running-man-2010-episode-695.html";
     await page.goto(link,{timeout: 0,waitUntil: 'networkidle2'});
     await page.waitForSelector('.watch_video > iframe');
+    console.log('Selector found: .watch_video > iframe');
     await page.click('.watch_video > iframe');
+    console.log('Selector clicked');
+    res.send(`worked: ${e}`);
   } catch (e) {
     console.error(e);
     res.send(`Something went wrong while running Puppeteer: ${e}`);
